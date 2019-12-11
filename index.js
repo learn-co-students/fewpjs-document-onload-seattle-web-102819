dc32ce972257b95c80a22a334e901c3bea123c74
bahayg <bahayg@gmail.com>
2019-12-11 11:54:41 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('text').textContent = "This is really cool!"
+    document.getElementById('text').textContent = "This is really cool!";
 })
+
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
+  
+  
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
